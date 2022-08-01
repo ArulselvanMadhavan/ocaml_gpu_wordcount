6685b60c78def982ebedc9002ea7d7621195705a
Arulselvan Madhavan <arulselvan1234@gmail.com>
2022-08-01 15:21:31 -0700

    C example with bigarray cast

--- bin/main.ml
+++ bin/main.ml
@@ -1,21 +1,23 @@
 open Ocaml_gpu_wordcount
 
+let load_file filename =
+  let fd = Unix.openfile filename [Unix.O_RDONLY] 0o666 in
+  let genarray = Unix.map_file fd ~pos:0L Bigarray.int8_unsigned Bigarray.c_layout false [|-1|] in
+  let u8_array1 = Bigarray.array1_of_genarray genarray in
+  let result = Ctypes.bigarray_start Ctypes_static.Array1 u8_array1 in
+  let u8ptr = Ctypes.coerce (Ctypes.ptr Ctypes.int) (Ctypes.ptr Ctypes.uint8_t) result in  
+  Bigarray.Array1.dim u8_array1, u8ptr
+    
 let () =
   let i = C.Function.futhark_get_tuning_param_count () in
   let cfg = C.Function.futhark_context_config_new () in
   let ctx = C.Function.futhark_context_new cfg in
   let _arr = C.Function.futhark_new_u8_1d ctx in
-  let f c = Char.code c |> Unsigned.UInt8.of_int in
-  let char_arr = Array.map f @@ Base.String.to_array "example from futhark\nline2\nline3\n" in
-  (* Array.iter (fun c -> Printf.printf "%d|" (Unsigned.UInt8.to_int c)) char_arr;
-   * print_endline "done"; *)
-  (* Char.code *)
-  let data_ptr =
-    Ctypes.CArray.start
-    @@ Ctypes.CArray.of_list Ctypes.uint8_t
-    @@ Array.to_list char_arr
+  let size, data_ptr = load_file "huge.txt" in
+  let arr =
+    C.Function.futhark_new_u8_1d ctx data_ptr
+      (Int64.of_int size)
   in
-  let arr = C.Function.futhark_new_u8_1d ctx data_ptr (Int64.of_int @@ Array.length char_arr) in
   let chars = Ctypes.allocate Ctypes.int32_t Int32.zero in
   let words = Ctypes.allocate Ctypes.int32_t Int32.zero in
   let lines = Ctypes.allocate Ctypes.int32_t Int32.zero in
@@ -23,6 +25,6 @@ let () =
   let _ = C.Function.futhark_free_u8_1d ctx arr in
   C.Function.futhark_context_free ctx;
   C.Function.futhark_context_config_free cfg;
-  let results = List.map Ctypes.(!@) [chars;words;lines] in
+  let results = List.map Ctypes.( !@ ) [ chars; words; lines ] in
   List.iter (fun i -> Printf.printf "%d\n" @@ Int32.to_int i) results;
   Printf.printf "Futhark param count:%d|%d\n" i result
